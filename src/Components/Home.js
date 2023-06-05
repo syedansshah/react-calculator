552796fd6c0622d1e80dd602d0b4699f88d153d8
ans <syed.shah@safarifoneict.com>
2023-06-05 17:50:21 +0500

    - added basic calculator outer structure

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./Style/Home.css"
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className='home'>
+            <div className='inner'>
+                <div className='display'>
+                    <div className='result'>
+1
+                    </div>
+                </div>
+            </div>
+            <div className='lower'>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
