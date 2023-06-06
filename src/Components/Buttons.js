8eced2e6d18200b136d07ed248272080bdf3991c
ans <syed.shah@safarifoneict.com>
2023-06-06 14:57:53 +0500

    - added the calculator functionality

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import classes from './Style/Button.module.css'
 
-function Buttons({value}) {
+function Buttons({value, handler}) {
   return (
-    <div className={value==="C" ||  value==="=" ? `${classes.buttons} ${classes.result}` : `${classes.buttons}` }>{value}</div>
+    <div 
+    className={value==="C" ||  value==="=" ? `${classes.buttons} ${classes.result}` 
+    :`${classes.buttons}` }
+    onClick={()=>handler(value)}>
+    {value}
+    </div>
   )
 }
 
